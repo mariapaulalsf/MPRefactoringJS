912fcbb342e4259d2b6da1d189e111f3545b90df
MARIA PAULA LACERDA SANTOS FONSECA <0083470@ifmg.edu.br>
2026-06-15 10:25:43 -0300

    Commit 9 - Criando Arquivos

--- index.js
+++ index.js
@@ -1,82 +1,9 @@
 const { readFileSync } = require('fs');
-class Repositorio {
-  constructor() {
-    this.pecas = JSON.parse(readFileSync('./pecas.json'));
-  }
+var Repositorio = require("./repositorio.js");
+var ServicoCalculoFatura = require("./servico.js") ;
+var gerarFaturaStr = require("./apresentacao.js");
 
-  getPeca(apre) {
-    return this.pecas[apre.id];
-  }
-}
-
-function formatarMoeda(valor) {
-  return new Intl.NumberFormat("pt-BR",
-    { style: "currency", currency: "BRL",
-      minimumFractionDigits: 2 }).format(valor / 100);
-}
-
-class ServicoCalculoFatura {
-  constructor(repo) {
-    this.repo = repo;
-  }
-
-  calcularTotalApresentacao(apre) {
-    let total = 0;
-    switch (this.repo.getPeca(apre).tipo) {
-      case "tragedia":
-        total = 40000;
-        if (apre.audiencia > 30) {
-          total += 1000 * (apre.audiencia - 30);
-        }
-        break;
-      case "comedia":
-        total = 30000;
-        if (apre.audiencia > 20) {
-           total += 10000 + 500 * (apre.audiencia - 20);
-        }
-        total += 300 * apre.audiencia;
-        break;
-      default:
-          throw new Error(`Peça desconhecida: ${this.repo.getPeca(apre).tipo}`);
-    }
-    return total;
-  }
-
-  calcularCredito(apre) {
-    let creditos = 0;
-    creditos += Math.max(apre.audiencia - 30, 0);
-    if (this.repo.getPeca(apre).tipo === "comedia") 
-       creditos += Math.floor(apre.audiencia / 5);
-    return creditos;   
-  }
-
-  calcularTotalFatura(apresentacoes) {
-    let total = 0;
-    for (let apre of apresentacoes) {
-      total += this.calcularTotalApresentacao(apre); 
-    }
-    return total;
-  }
-
-  calcularTotalCreditos(apresentacoes) {
-    let totalCreditos = 0;
-    for (let apre of apresentacoes) {
-      totalCreditos += this.calcularCredito(apre); 
-    }
-    return totalCreditos;
-  }
-}
-function gerarFaturaStr(fatura, calc) {
-  let faturaStr = `Fatura ${fatura.cliente}\n`;
-  for (let apre of fatura.apresentacoes) {
-      faturaStr += `  ${calc.repo.getPeca(apre).nome}: ${formatarMoeda(calc.calcularTotalApresentacao(apre))} (${apre.audiencia} assentos)\n`;
-  }
-  faturaStr += `Valor total: ${formatarMoeda(calc.calcularTotalFatura(fatura.apresentacoes))}\n`;
-  faturaStr += `Créditos acumulados: ${calc.calcularTotalCreditos(fatura.fatura.apresentacoes || fatura.apresentacoes)} \n`;
-  return faturaStr;
-}
 const faturas = JSON.parse(readFileSync('./faturas.json'));
-
 const calc = new ServicoCalculoFatura(new Repositorio());
 const faturaStr = gerarFaturaStr(faturas, calc);
 console.log(faturaStr);
