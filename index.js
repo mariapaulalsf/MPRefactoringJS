e98dee260ebdfec5b78a90713ca562e823913611
MARIA PAULA LACERDA SANTOS FONSECA <0083470@ifmg.edu.br>
2026-06-15 10:27:05 -0300

    Commit 9 - Criando Arquivos

--- index.js
+++ index.js
@@ -7,3 +7,4 @@ const faturas = JSON.parse(readFileSync('./faturas.json'));
 const calc = new ServicoCalculoFatura(new Repositorio());
 const faturaStr = gerarFaturaStr(faturas, calc);
 console.log(faturaStr);
+//
